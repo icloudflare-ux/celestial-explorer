22b3970b5ddbba943a4c2f18c3b2b67cc9d9ac37
icloudflare <cloudflarei388@gmail.com>
2025-12-19 01:52:06 +0330

    fix: change base path to relative

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,8 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, (process as any).cwd(), '');
 
   return {
-    // توجه: به جای REPO_NAME باید نام دقیق ریپازیتوری جدیدی که در گیت‌هاب می‌سازید را بنویسید
-    // مثلا اگر نام ریپازیتوری my-astrology است، این خط باید بشود: base: '/my-astrology/',
-    base: '/REPO_NAME/', 
+    // تغییر از '/REPO_NAME/' به './' برای حل مشکل لود نشدن فایل‌ها در نتلیفای
+    base: './', 
     define: {
       'process.env.API_KEY': JSON.stringify(env.API_KEY),
     },
