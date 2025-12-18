7f8618aad79e15f81b5b7d616cc38acfdf6093ce
icloudflare <cloudflarei388@gmail.com>
2025-12-19 01:59:42 +0330

    fix: base path and translation files

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, (process as any).cwd(), '');
 
   return {
-    // تغییر از '/REPO_NAME/' به './' برای حل مشکل لود نشدن فایل‌ها در نتلیفای
-    base: './', 
+    // تغییر به '/' برای اطمینان از پیدا شدن فایل‌های JSON و دارایی‌های سایت در Netlify
+    base: '/', 
     define: {
       'process.env.API_KEY': JSON.stringify(env.API_KEY),
     },
